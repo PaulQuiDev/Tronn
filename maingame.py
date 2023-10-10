ef0ab95fbe726fe2e7aac35229f91c294204e1c5
Mira1400 <maelle.roche@edu.devinci.fr>
2023-10-10 18:20:58 +0200

    functionnal version

--- maingame.py
+++ maingame.py
@@ -1,8 +1,50 @@
-rows, cols = (32,32)
-tab = [[0 for i in range(cols)] for j in range(rows)]
-for i in range(cols):
-    for j in range(rows):
-        print(str(tab[i][j])+" ")
-    print()
 
-startPointJ1=(17,0)
+#initialize board size
+rows, cols = (35,35)
+tab = [[0 for i in range(cols)] for j in range(rows)]
+
+#fix board boarders
+for i in range(cols):
+    tab[0][i]='_'
+    tab[(rows-1)][i]='_'
+for j in range(rows):
+    tab[j][0]='_'
+    tab[j][(cols-1)]='_'
+
+# initialize start point for each players
+startPointJ1=(17,1)
+startPointJ2=(1,17)
+startPointJ3=(17,rows-2)
+startPointJ4=(cols-2,17)
+
+#= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+# function : print board in terminal
+def affichage(tab):
+    for i in range(cols):
+        for j in range(rows):
+            if tab[i][j]==0:
+                print(". ",end="")
+            else :
+                print(str(tab[i][j])+" ",end="")
+        print()
+
+# function: check if the case are available, add a mouve markdown
+def jouer(player,x,y,tab):
+    if tab[x][y]==0 :
+        tab[x][y]=player
+        return True
+    else:
+        return False
+    
+def erase(player,tab):
+    for i in range(rows):
+        for j in range(cols):
+            if tab[i][j]==player:
+                tab[i][j]=0
+    
+    
+#= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+# print tab in terminal
+affichage(tab)
--- test_pygame.py
+++ test_pygame.py
@@ -1,48 +1,194 @@
 
 import pygame 
 from pygame.locals import *
+import sys
+import multiprocessing
+import maingame
+import CliReso
 
-# initializing imported module 
+# initializing imported module, clock and colors
 pygame.init() 
-  
-# displaying a window of height 
-# 500 and width 400 
-surface = pygame.display.set_mode((1500, 1000)) 
-
-# Initializing RGB Color
+clock = pygame.time.Clock()
+color =(0,0,0)
+start=(0,0)
 bkg = (150,150,150)
 mainc=(40, 40, 40)
-
+line=(200,200,255)
+answer = multiprocessing.Queue()
+  
+# displaying a window and background
+surface = pygame.display.set_mode((1505, 1010)) 
 surface.fill(bkg)
 
+#= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+# function : initialize start players position
+def debut(player):
+    
+    if player==1:
+        color=(255,0,0)
+        start = (int(1485*maingame.startPointJ1[0]/33)-5,int(990*maingame.startPointJ1[1]/33))
+        startF= (start[0]-5,start[1]-5)
+
+    if player == 2 :
+        color =(0,255,0)
+        start= (int(1485*maingame.startPointJ2[0]/33),int(990*maingame.startPointJ2[1]/33)+5)
+        startF= (start[0]-5,start[1]-5)
+
+    if player == 3:
+        color =(255,255,0)
+        start = (int(1485*maingame.startPointJ3[0]/33)-5,int(990*maingame.startPointJ3[1]/33)+5)
+        startF= (start[0]-5,start[1]-2)
+        print("initial")
+
+    if player ==4:
+        color=(0,0,255)
+        start= (int(1485*maingame.startPointJ4[0]/33),int(990*maingame.startPointJ4[1]/33)+5)
+        startF= (start[0]-5,start[1]-5)
+       
+    pygame.draw.rect(surface,color,pygame.Rect((start[0]-10,start[1]-10),(10,10)))
+    pygame.display.flip()
+  
+    return startF, color
+
+
+#function : mouve one player in a specified direction
+def avancer(player,x,y,direction,color):
+    #define numer of pixels for an horizontal (x) or vertical (y) mouve
+    xunit = 45
+    yunit=30
+    if direction =="+x":
+        newx= x+xunit
+        newy = y
+        maingame.affichage(maingame.tab)
+    if direction =="-x":
+        newx=x-xunit
+        newy = y
+        maingame.affichage(maingame.tab)
+    if direction =="+y":
+        newx = x
+        newy= y+yunit
+        maingame.affichage(maingame.tab)
+    if direction =="-y":
+        newx=x
+        newy=y-yunit
+        maingame.affichage(maingame.tab)
+
+    #bloc the line in the window
+    if newx>1495:
+        newx=1495
+    elif newx<10 :
+        newx =10
+    if newy >1000:
+        newy=1000
+    elif newy<10:
+        newy=10
+
+    #draw mouve
+    pygame.draw.line(surface,color,(x,y),(newx,newy),width=5)
+    pygame.display.flip()
+    return newx,newy # return new current position
+
+#function: define new direction acording to the current direction
+def changementDir (old,newdir):
+    new="+y"
+    if old=="+y":
+        if newdir =='D':
+            new ="+x"
+        elif newdir=='G':
+            new="-x"
+    elif old=="-y":
+        if newdir =='D':
+            new ="+x"
+        elif newdir=='G':
+            new="-x"
+    elif old=="+x":
+        if newdir =='H':
+            new ="-y"
+        elif newdir=='B':
+            new="+y"
+    elif old=="-x":
+        if newdir =='H':
+            new ="-y"
+        elif newdir=='B':
+            new="+y"
+
+    return new
+
+#function : convert a pixel position in an integer index
+def Xconvert_px_to_index(xpx):
+    if player==3:
+        x=int(xpx*33/1485)
+    else:
+        x=int(xpx*33/1485)+1
+    return x
+def Yconvert_px_to_index(ypx):
+    if player==3:
+        y=int(ypx*33/990)
+    else:
+        y=int(ypx*33/990)+1
+    return y
+
+#= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
    
-# Drawing Rectangle
-pygame.draw.rect(surface, mainc,pygame.Rect(10, 10, 1480, 980))
+# Drawing background grid
+pygame.draw.rect(surface, mainc,pygame.Rect(10, 10, 1485, 990))
+for i in range(10,1485,45):
+    pygame.draw.line(surface,line,(i,10),(i,1000))
+pygame.draw.line(surface,line,(1495,10),(1495,1000))
 
-'''fond=pygame.image.load("./image/background.jpg").convert()
-fond=pygame.transform.scale(fond,(1480,980))
-surface.blit(fond,(10,10))'''
+for i in range(10,991,30):
+    pygame.draw.line(surface,line,(10,i),(1495,i))
+pygame.draw.line(surface,line,(10,1495),(1000,1495))
 
- 
-# Changing surface color
-pygame.display.flip()
+pygame.display.flip() #refresh rendering
   
-# creating a bool value which checks 
-# if game is running 
-running = True
+
+# creat setup variable and initialize game
+player=3
+start,color = debut(player)
+x,y=start
+globalDirection = "-y"
+
+#= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
   
-# keep game running till running is true 
-while running: 
-      
-    # Check for event if user has pushed 
-    # any event in queue 
-    for event in pygame.event.get(): 
-          
-        # if event is of type quit then  
-        # set running bool to false 
+# Game Loop
+while True: 
+
+    clock.tick(10) #manage fps rate 
+
+    # Check for event if user has pushed any event
+    for event in pygame.event.get():   
+
+        # quit event
         if event.type == pygame.QUIT: 
+            pygame.quit()
+            sys.exit()
+
+        #user key press event
+        if event.type == pygame.KEYDOWN:
+            #manage vertical mouve
+            if globalDirection=="+y" or globalDirection=="-y":
+                if event.key == K_RIGHT:
+                    globalDirection=changementDir(globalDirection,"D")
+                if event.key == K_LEFT:
+                    globalDirection=changementDir(globalDirection,"G")
+
+            #manage horizontal mouve
+            elif globalDirection=="+x" or globalDirection=="-x":
+                if event.key == K_UP:
+                    globalDirection=changementDir(globalDirection,"H")
+                if event.key == K_DOWN:
+                    globalDirection=changementDir(globalDirection,"B")
+    
+    CliReso.ConnectionClient(globalDirection,answer)
+    update= answer.get()
+    
+
+    # look the save matrix (maingame.tab), if case are free, move forward
+    if maingame.jouer(player,Yconvert_px_to_index(y),Xconvert_px_to_index(x), maingame.tab):
+        x,y=avancer(player,x,y,globalDirection,color)
+
+
         
-            if event.key == K_RIGHT:
-                print("droite")
-            if event.key == K_LEFT:
-                print("gauche")
+
